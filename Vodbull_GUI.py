ba3253984fcc1d515a39a36007c9d68dc20d598a
Neil Gavin <liamgoulding03@gmail.com>
2022-05-05 14:34:37 +0100

    Added GUI

---
+++ Vodbull_GUI.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+from Vodbull_Buttons import showerror, showwarning, showinfo
+
+window = tk.Tk()
+greeting = tk.Label(Vodbull .CSV Cleaner)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+greeting.pack()
+
+
