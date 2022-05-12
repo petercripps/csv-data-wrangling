c6862b7302f2ddafc37cdfaaab27d63d623e7076
Peter Cripps <petercripps@icloud.com>
2022-05-12 23:00:31 +0100

    Added quit button

--- Vodbull_GUI.py
+++ Vodbull_GUI.py
@@ -7,24 +7,45 @@ frame = tk.Frame()
 frame.pack()
 greeting = tk.Label(text="Vodbull .CSV Cleaner")
 
+def wrangle_callback():
+    print("Wrangle pressed")
+
 wrangle_button = tk.Button(
     text="Wrangle",
+    command=wrangle_callback,
     width=10,
     height=1,
     bg="grey",
     fg="black"
 )
+
+def sort_callback():
+    print("Sort pressed")
 
 sort_button = tk.Button(
     text="Sort",
+    command=sort_callback,
     width=10,
     height=1,
     bg="grey",
     fg="black"
 )
 
+def help_callback():
+    print("Help pressed")
+
 help_button = tk.Button(
     text="Help",
+    command=help_callback,
+    width=10,
+    height=1,
+    bg="grey",
+    fg="black"
+)
+
+quit_button = tk.Button(
+    text="Quit",
+    command=window.destroy,
     width=10,
     height=1,
     bg="grey",
@@ -35,5 +56,6 @@ greeting.pack()
 wrangle_button.pack()
 sort_button.pack()
 help_button.pack()
+quit_button.pack()
 
 window.mainloop()
