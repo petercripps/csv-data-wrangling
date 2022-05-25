caeb9c2a61ddd6bef8e5248ac258c66cb6bb98b0
Liam <Liam@Digitalinnovators.co.uk>
2022-05-25 13:27:03 +0100

    Added file input to UI

--- vodbullGUI.py
+++ vodbullGUI.py
@@ -1,17 +1,29 @@
 
+from ast import Constant
+from cgitb import text
 from itertools import count
+from logging import root
+from msilib.schema import File
 import tkinter as tk
 from tkinter import EXTENDED, ttk
 from tkinter import *
-from time import strftime 
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+import os
+from time import strftime
+from turtle import width
+from numpy import blackman 
 import yaml
 from csvwrangle import run_csv_wrangle
 from readargs import read_yaml_file
+import os
 
 window = tk.Tk()
 frame = tk.Frame()
 frame.pack()
 greeting = tk.Label(text="Vodbull .CSV Cleaner")
+window.geometry("600x300")
+
 
 def wrangle_callback():
     configdict = read_yaml_file("config.yaml")
@@ -58,6 +70,33 @@ help_button = tk.Button(
     bg="grey",
     fg="black"
 )
+        
+
+def file_select():
+    file = filedialog.askopenfile(mode='r', filetypes=[('CSV Files', '*.csv')])
+    global filepath
+    if file:
+        filepath = os.path.abspath(file.name)
+        file_label = tk.Label(
+            text=filepath,
+            fg="black",
+            bg="#F2F2F2",
+            width=40,
+            height=4,
+).pack()
+    
+    
+
+file_button = tk.Button(
+    text="File Select",
+    width=10,
+    height=1,
+    command=file_select,
+    bg="grey",
+    fg="black"
+)
+
+
 
 quit_button = tk.Button(
     text="Quit",
@@ -68,14 +107,10 @@ quit_button = tk.Button(
     fg="black"
 )
 
-
-
-
-
 greeting.pack()
 wrangle_button.pack()
 sort_button.pack()
+file_button.pack()
 help_button.pack()
 quit_button.pack()
-
 window.mainloop()
